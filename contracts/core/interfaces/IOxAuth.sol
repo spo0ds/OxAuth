a7fbb3aadc836df1affc1870cee54de569f2ff83
spo0ds <asmitadhikari540@gmail.com>
2023-01-08 23:23:01 +0545

    interface for OxAuth

---
+++ contracts/core/interfaces/IOxAuth.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+interface IOxAuth {
+    event ApproveRequest(
+        address indexed requester,
+        address indexed to,
+        string indexed data,
+        uint timePeriod
+    );
+
+    event AccessGrant(
+        address indexed approver,
+        address indexed thirdParty,
+        string indexed data,
+        uint timePeriod
+    );
+
+    event GrantRevoke(
+        address indexed approver,
+        address indexed requester,
+        string data
+    );
+
+    function requestApprove(
+        address walletAddress,
+        string memory data,
+        uint timePeriod
+    ) external;
+
+    function grantAccess(
+        address approver,
+        address thirdParty,
+        string memory data
+    ) external;
+
+    function viewData(
+        address walletAddress,
+        string memory data
+    ) external returns (bool);
+
+    function revokeGrant(address thirdParty, string memory data) external;
+}
