550afb0b3467fb61ee9c7d637cf229e368bd3311
spo0ds <asmitadhikari540@gmail.com>
2023-01-20 23:19:19 +0545

    test for mint nft added

--- test/NTNFT.specs.js
+++ test/NTNFT.specs.js
@@ -1,11 +1,14 @@
-const { expect } = require("chai")
+const { expect, assert } = require("chai")
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers")
 const { ethers } = require("hardhat")
-describe("NTNFT Contract", () => {
-    async function deployNftFixture() {
 
-        const nftContract = await ethers.getContractFactory("NTNFT")
-        const nft = await nftContract.deploy()
+
+describe("NTNFT Contract", () => {
+    let accounts, deployer, nft
+
+    async function deployNftFixture() {
+        const nftContract = await ethers.getContractFactory("NtNft")
+        nft = await nftContract.deploy()
         await nft.deployed()
 
         return {
@@ -13,15 +16,20 @@ describe("NTNFT Contract", () => {
         }
     }
 
-    describe("Mint an NFT", () => {
-        it("after the mint, it should increase the token counter of the NFT", async () => {
+    describe("Mint NFT", () => {
+        async function mintFixture() {
+            accounts = await ethers.getSigners()
+            deployer = accounts[0].address
             const { nft } = await loadFixture(deployNftFixture)
-            const { oldTokenCounter } = await nft.getTokenCounter()
-            const txResponse = await nft.mintNft()
+            const txResponse = await nft.mintNft(deployer)
             await txResponse.wait(1)
-            const { newTokenCounter } = await nft.getTokenCounter()
-            expect(newTokenCounter?.toString()).eq("1");
-            expect(oldTokenCounter?.toString()).eq("0");
+        }
+
+        it("Allows users to mint an NFT, and updates appropriately", async function () {
+            await loadFixture(mintFixture)
+            const tokenURI = await nft.tokenURI(0)
+            const tokenCounter = await nft.getTokenCounter()
+            assert.equal(tokenCounter.toString(), "1")
         })
     })
 }) 
