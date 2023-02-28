6370dbf7fdc7f185a240185501fa2f97f2c7dce0
leon0X01 <ushrestha358@gmail.com>
2023-02-28 13:21:17 +0545

    added natspec of KYC.sol

--- contracts/core/KYC.sol
+++ contracts/core/KYC.sol
@@ -2,23 +2,43 @@
 
 pragma solidity 0.8.17;
 
-import "./interfaces/IKYC.sol";
-import "./libraries/Types.sol";
-import "./interfaces/IOxAuth.sol";
-import "./NTNFT.sol";
-import "./OxAuth.sol";
+import {IKYC} from"./interfaces/IKYC.sol";
+import {Types} from"./libraries/Types.sol";
+import {IOxAuth} from "./interfaces/IOxAuth.sol";
+import {NTNFT} from "./NTNFT.sol";
+import {OxAuth} from "./OxAuth.sol";
+
+/*///////////////////////////////////////////////////////////////////////////////
+                           CUSTOME ERROR 
+//////////////////////////////////////////////////////////////////////////////*/
 
 error KYC__INVALIDSignatureLength();
 error KYC__CannotViewData();
 error KYC__DataDoesNotExist();
-error KYC__FieldDoesNotExist();
 
 contract KYC is IKYC, OxAuth {
+    /// @notice This mapped the user details according to their address
     mapping(address => Types.UserDetail) private s_userInfo;
+
+    /// @notice Mapped hased of User details to its address 
     mapping(address => bytes32) private s_hashedData;
 
-    OxAuth oxAuth;
 
+    /*///////////////////////////////////////////////////////////////////////////////
+                           SetUserDATA 
+    //////////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice SetUserData is used to set User data and mapped to it address
+    /// @param  _name User's Name
+    /// @param  _father_name User's Father Name
+    /// @param  _mother_name User's Mother Name 
+    /// @param  _grandFather_name User's GrandFather Name
+    /// @param  _phone_number User's Phone_Number details
+    /// @param  _dob User's Date of Birth 
+    /// @param  _blood_group User's Blood Group
+    /// @param  _Citizenship_number User's CitizenShip_number
+    /// @param  _pan_number User's pan Number 
+    /// @param  _location User's Location 
     function setUserData(
         string memory _name,
         string memory _father_name,
@@ -31,6 +51,9 @@ contract KYC is IKYC, OxAuth {
         string memory _pan_number,
         string memory _location
     ) external {
+
+        //mapped deployer address and provide their details 
+
         s_userInfo[msg.sender] = Types.UserDetail(
             _name,
             _father_name,
@@ -45,11 +68,23 @@ contract KYC is IKYC, OxAuth {
             false
         );
     }
+    /*///////////////////////////////////////////////////////////////////////////////
+                         GetMessageHash                           
+    //////////////////////////////////////////////////////////////////////////////*/
 
+    /// @notice GetMessageHash is used to hash User details using Keccak 256
+    /// @param dataProviderAddress address of the KycDataProvider
+    /// @return bytes32 Keccak256 hash value of the User details
     function getMessageHash(
         address dataProviderAddress
     ) private view returns (bytes32) {
+
+        // access the struct datatypes from Types.UserDetail
+
         Types.UserDetail memory userData = s_userInfo[dataProviderAddress];
+
+        // perform keccak256 and produce the unique has of represent data
+
         return
             keccak256(
                 abi.encodePacked(
@@ -66,9 +101,16 @@ contract KYC is IKYC, OxAuth {
             );
     }
 
-    function getEthSignedMessageHash(
-        address dataProviderAddress
-    ) private returns (bytes32) {
+    /*///////////////////////////////////////////////////////////////////////////////
+                           getEthSignedMessageHash 
+    //////////////////////////////////////////////////////////////////////////////*/
+
+
+    /// @notice getEtheSignedMessageHash allows to generate signature that is signed by data Provider Address 
+    /// @param dataProviderAddress It is address of the User who filled the Kyc details 
+    /// @dev generate the Signature that proves the particular accound signed the message
+    /// @return the Bytes32 address of the signature which represent the address signed the message
+    function getEthSignedMessageHash(address dataProviderAddress) private returns (bytes32) {
         /*
         Signature is produced by signing a keccak256 hash with the following format:
         "\x19Ethereum Signed Message\n" + len(msg) + msg
@@ -79,43 +121,84 @@ contract KYC is IKYC, OxAuth {
                 getMessageHash(dataProviderAddress)
             )
         );
+        
+        // mapped the bytes32 hashed signature to  data provider address in storage variable
+        
         s_hashedData[dataProviderAddress] = data;
 
         return data;
     }
 
-    function generateHash(
-        address dataProviderAddress
-    ) external override returns (bytes32) {
+
+    /*///////////////////////////////////////////////////////////////////////////////
+                                 generateHash 
+    //////////////////////////////////////////////////////////////////////////////*/
+
+
+    /// @notice generateHash is call the getEthSignedMessageHash and retun the hashed signature of
+    /// @param dataProviderAddress It is address of the User who filled the Kyc details
+    /// @return message digest of signature 
+    function generateHash(address dataProviderAddress) external override returns (bytes32) {
+
+        // call the getEtheSignedMessageHash function and generate Ethereum signed message and KYC details 
         return getEthSignedMessageHash(dataProviderAddress);
     }
 
-    function verify(
-        address dataProviderAddress,
-        bytes memory signature
+    /*///////////////////////////////////////////////////////////////////////////////
+                                  VERIFY
+    //////////////////////////////////////////////////////////////////////////////*/
+
+
+    /// @notice Verify whether the Kyc details is signed by the right DataProviderAddress 
+    /// @param dataProviderAddress It is address of the User who filled the Kyc details
+    /// @return bool true if it signed by correct signer or false
+    function verify(address dataProviderAddress,bytes memory signature
     ) external override returns (bool) {
+
+
+        // get the ethSignedMessageHash from signer address ie dataProvideraddress
         bytes32 ethSignedMessageHash = getEthSignedMessageHash(
             dataProviderAddress
         );
-        if (
-            recoverSigner(ethSignedMessageHash, signature) ==
-            dataProviderAddress
-        ) {
+
+        // Check whethere EthsginedMessageHash and signature represent the rigth Kyc data Provider
+        if (recoverSigner(ethSignedMessageHash, signature) == dataProviderAddress) {
             return s_userInfo[dataProviderAddress].isVerified = true;
-        } else {
+        } 
+        else {
             return false;
         }
     }
 
+
+    /*///////////////////////////////////////////////////////////////////////////////
+                              RECOVER Signer 
+    //////////////////////////////////////////////////////////////////////////////*/
+
+    
+    /// @notice recoverSigner allows the SC to validate the incoming data is properly signed
+    /// @param _ethSignedMessageHash It is SignedMessageHash of ethereum signature and Kyc details
+    /// @param _signature It is the signature of Kyc details signed 
+    /// @return address is return from ecrecover
     function recoverSigner(
         bytes32 _ethSignedMessageHash,
         bytes memory _signature
     ) internal pure returns (address) {
+        
+        // seperate into r, s, v to pass in ecrecover 
         (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
 
         return ecrecover(_ethSignedMessageHash, v, r, s);
     }
 
+    /*///////////////////////////////////////////////////////////////////////////////
+                              RECOVER Signer 
+    //////////////////////////////////////////////////////////////////////////////*/
+
+
+    /// @notice splitSignature is used to extract `r`, `s` and `v` values from a signature passed as byte array
+    /// @param sig this is signature that represent the exact dataprovider who signed the message
+    /// @dev perform signature split r+v+s => r=> 32byte , v=>32byte, s=1byte
     function splitSignature(
         bytes memory sig
     ) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
@@ -143,17 +226,39 @@ contract KYC is IKYC, OxAuth {
         // implicitly return (r, s, v)
     }
 
+
+    /*///////////////////////////////////////////////////////////////////////////////
+                              RECOVER Signer 
+    //////////////////////////////////////////////////////////////////////////////*/
+
+
+    /// @notice getEthhashedData is getter function which react the hashData from storage variable who is mapped through address
+    /// @param dataProviderAddress It is the signer of Kyc data or KYC provider.
+    /// @return messageDigest of KYC_details 
     function getEthHashedData(
         address dataProviderAddress
     ) external view returns (bytes32) {
         return s_hashedData[dataProviderAddress];
     }
 
+    /*///////////////////////////////////////////////////////////////////////////////
+                               Get User Data 
+    //////////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice GEtUserDAta is function who provide the specific KYC datg to user who request to See Details.
+    /// @param  dataProvider It is address where dataRequestor is requesting to view the data
+    /// @param  data This represent the specific field of KYC form such as name, dob and so forth
+    /// @retun return the string datatype of specific field of KYC from 
+
+    /// Requestor first need to get approved to view data
     function getUserData(
         address dataProvider,
         string memory data
     ) external view returns (string memory) {
-        require((OxAuth._Approve[dataProvider][msg.sender][data] || msg.sender == dataProvider)==true, "not access yet");
+        require(
+            OxAuth._Approve[dataProvider][msg.sender][data] == true,
+            "not access yet"
+        );
 
         if (keccak256(abi.encode("name")) == keccak256(abi.encode(data))) {
             return s_userInfo[dataProvider].name;
